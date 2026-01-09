621fef9daf31c41f91fdcab669e13d6562e42b6d
Riccardo Calabri <153217211+777riki@users.noreply.github.com>
2026-01-09 20:28:30 +0000

    fix chiusura navbar, fix grid capolavori

--- isc-tattoo-studio/src/app/components/navbar/navbar.ts
+++ isc-tattoo-studio/src/app/components/navbar/navbar.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+declare global {
+  interface Window {
+    bootstrap: any;
+  }
+}
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +14,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar implements OnInit {
+export class Navbar {
 
   ngOnInit(): void {
     window.scroll(0,0);
   }
 
+  closeNavbar() {
+    const navbar = document.getElementById('navbarNavDropdown');
+
+    if (navbar && navbar.classList.contains('show')) {
+      const bsCollapse =
+        window.bootstrap.Collapse.getInstance(navbar) ||
+        new window.bootstrap.Collapse(navbar, { toggle: false });
+
+      bsCollapse.hide();
+    }
+  }
 }
