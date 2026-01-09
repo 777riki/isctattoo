00c2954adaf3e60cff5cc4fc90fbb2fc5d3c93f5
Riccardo Calabri <153217211+777riki@users.noreply.github.com>
2026-01-09 21:45:20 +0000

    fix finale

--- isc-tattoo-studio/src/app/components/prenotazioni/prenotazioni.ts
+++ isc-tattoo-studio/src/app/components/prenotazioni/prenotazioni.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Mail } from '../../services/mail';
 import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-prenotazioni',
@@ -10,11 +10,16 @@ import { FormsModule } from '@angular/forms';
 })
 export class Prenotazioni implements OnInit {
 
+  showAlert: boolean = false;
+  alertMessage: string = '';
+
+  private service_id = "service_fudclb9";
+  private template_id = "template_0dypajd";
+  private public_key = "Uw3j_4-L01jNtTBjo";
+
   ngOnInit(): void {
     window.scroll(0,0);
   }
-
-  mailService: Mail = inject(Mail);
 
   name: string = '';
   email: string = '';
@@ -23,8 +28,46 @@ export class Prenotazioni implements OnInit {
   position: string = '';
   notes: string = '';
 
-  sendEmail() {
-    this.mailService.sendEmail(this.name, this.email, this.phone, this.dimension, this.position, this.notes);
+  async sendEmail() {
+    if (!this.name || !this.email || !this.phone || !this.dimension || !this.position) {
+      this.alertMessage = 'Devi compilare tutti i campi!';
+      this.showAlert = true;
+      return;
+    };
+
+    try {
+      const response = await emailjs.send(
+        this.service_id,
+        this.template_id,
+        {
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+          dimension: this.dimension,
+          position: this.position,
+          notes: this.notes
+        },
+        { publicKey: this.public_key }
+      );
+
+      // Reset form
+      this.name = '';
+      this.email = '';
+      this.phone = '';
+      this.dimension = '';
+      this.position = '';
+      this.notes = '';
+
+      this.alertMessage = 'Email inviata con successo!';
+      this.showAlert = true;
+    } catch (error) {
+      this.alertMessage = 'Qualcosa è andato storto. Riprova più tardi.';
+      this.showAlert = true;
+    }
+  }
+
+  closeAlert() {
+    this.showAlert = false;
   }
 
 }
